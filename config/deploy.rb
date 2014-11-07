d0f1e3677342fb4c705362af918e8bc3e4b78ffe
Scot Dalton <scotdalton@gmail.com>
2014-11-07 17:35:19 -0500

    Remove deprecated Exlibris::Aleph refresh task

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,12 +10,3 @@ set :app_title, "eshelf"
 set :rvm_ruby_string, "jruby-1.7.16"
 set :stages, ["staging", "production"]
 set :recipient, "eshelf.admin@library.nyu.edu"
-
-namespace :exlibris do
-  namespace :aleph do
-    desc "Refresh Aleph tables"
-    task :refresh, :roles => :app do
-      run "cd #{current_release} && RAILS_ENV=#{rails_env} bundle exec rake exlibris:aleph:refresh"
-    end
-  end
-end
