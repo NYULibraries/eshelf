62a974f4ae99d8b5bddbebe4b2ba764efc7bbc8e
B Alter <barnaby.alter@gmail.com>
2015-05-19 21:28:38 -0400

    Do the same error handling for pnx as for xerxes_xml

--- app/models/primo.rb
+++ app/models/primo.rb
@@ -9,7 +9,11 @@ class Primo < Record
       self.title_sort = primo_record.sort_title if title_sort.blank?
       self.content_type = primo_record.display_type if content_type.blank?
       self.author = primo_authors.join("; ") if author.blank?
-      self.url = "#{to_openurl}" if url.blank?
+      begin
+        self.url = "#{to_openurl}" if url.blank?
+      rescue => e
+        self.url = "http://dev.getit.library.nyu.edu"
+      end
     end
   end
 
@@ -18,7 +22,7 @@ class Primo < Record
   def primo_locations
     unless primo_record.nil?
       @primo_locations ||= primo_record.holdings.collect do |holding|
-        { collection: "#{holding.library} #{holding.collection}", 
+        { collection: "#{holding.library} #{holding.collection}",
           call_number: holding.call_number }
       end
     end
@@ -30,15 +34,15 @@ class Primo < Record
   def primo_record
     # If we don't have data for Primo, call the web service.
     # Otherwise, create the record from that data.
-    @primo_record ||= (data.blank?) ? 
-      Exlibris::Primo::Search.new.record_id!(external_id).records.first : 
+    @primo_record ||= (data.blank?) ?
+      Exlibris::Primo::Search.new.record_id!(external_id).records.first :
         Exlibris::Primo::Record.new(raw_xml: data)
   end
   private :primo_record
 
   # Returns an array authors from the Exlibris::Primo::Record addata section
   def primo_authors
-    @primo_authors ||= 
+    @primo_authors ||=
       primo_record.all_addata_au.concat primo_record.all_addata_addau
   end
   private :primo_authors
