5ce277d100daeb756e938903aaee80163391c407
B Alter <barnaby.alter@gmail.com>
2015-06-17 15:45:11 -0400

    Changed method for loading tmp records again to not have a reject method which creates a duplicate array and so trying to save those records will indeed get a RecordNotUnique error

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,22 +8,16 @@ class UserSessionsController < ApplicationController
   # Intended to be called after validate on login
   def records_session_maintenance
     if (current_user and session[:tmp_user])
-      tmp_user_records = tmp_user.records.reject do |record|
-        current_user.records.where(external_system: record.external_system, external_id: record.external_id).present?
+      # Find all the tmp_users records
+      tmp_user.records.each do |record|
+        # Don't duplicate them if the current user already saved these records
+        unless current_user.records.where(external_system: record.external_system, external_id: record.external_id).present?
+          # But reassign them to this current user from the tmp user
+          record.tmp_user = nil
+          record.user = current_user
+          record.save!
+        end
       end
-      tmp_user_records.each do |record|
-        # This is a CRAZY hack.
-        # This can't really be necessary.
-        # Needs further investigation.
-        # Basically, ActiveRecords insists on making this an
-        # insert even though a simple key switch would do.
-        # record.id = nil
-        record.tmp_user = nil
-        record.user = current_user
-        record.save
-      end
-      # current_user.records.reload << tmp_user_records
-      current_user.save!
       # Get rid of the tmp user for the session
       session.delete(:tmp_user)
       # Reload from the DB, so we don't destroy the
