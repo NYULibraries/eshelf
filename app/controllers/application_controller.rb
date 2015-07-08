d0516569ad2e41a4ec47b44c40edfb4747e0d3b8
B Alter <barnaby.alter@gmail.com>
2015-07-08 14:52:39 -0400

    Added single sign out steps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,21 @@ class ApplicationController < ActionController::Base
   end
   alias :current_user :current_user_dev if Rails.env.development?
 
+  # Alias new_session_path as login_path for default devise config
+  def new_session_path(scope)
+    login_path
+  end
+
+  # After signing out from the local application,
+  # redirect to the logout path for the Login app
+  def after_sign_out_path_for(resource_or_scope)
+    if ENV['SSO_LOGOUT_URL'].present?
+      ENV['SSO_LOGOUT_URL']
+    else
+      super(resource_or_scope)
+    end
+  end
+
   # Returns the session's TmpUser
   # Sets the sessions' TmpUser if necessary.
   def tmp_user
