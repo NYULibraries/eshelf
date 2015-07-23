7aa2d1d98532872aa0a7f6e767a681fe26089f88
B Alter <barnaby.alter@gmail.com>
2015-07-23 18:14:26 -0400

    Redirect out to the account, its the only way without https

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,4 @@ module UsersHelper
   def link_to_tagged_records(tag)
     link_to tag, records_url(tag: tag.name)
   end
-  def aleph_account_url
-    URI.escape("#{Exlibris::Aleph.config.base_url}/F?func=bor-info")
-  end
 end
