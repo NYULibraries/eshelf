b662d5284c2bd8aa222deb632267731c7961a8da
B Alter <barnaby.alter@gmail.com>
2016-05-06 17:11:30 -0400

    Experimenting with why cookies aren't deleting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,14 @@ class UsersController < ApplicationController
 
   # Display the Aleph account for the current user.
   def account
-    return if performed?
     # Log into PDS directly since that is required
     # for the Aleph account option
-    if current_user.nil? || !cookies["PDS_HANDLE"]
+    if !cookies[:_pds_logged_in]
       cookies[:_return_to_account] = true
-      redirect_to pds_login and return
-    elsif !current_user.nil? && cookies["PDS_HANDLE"]
+      cookies[:_pds_logged_in] = true
+      redirect_to pds_login
+    elsif cookies[:_pds_logged_in]
+      cookies.delete(:_pds_logged_in, domain: "dev.eshelf.library.nyu.edu")
       respond_with(current_user)
     else
       head :bad_request
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -12,7 +12,7 @@ class UsersControllerTest < ActionController::TestCase
       (@user_record = @user_record.becomes_external_system).save!
     end
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    @request.cookies["PDS_HANDLE"] = "1234"
+    @request.cookies["_pds_logged_in"] = true
   end
 
   test "should get account" do
