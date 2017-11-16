1845774b176eb0f94a0c502a895f5cf1cf078403
hab278 <hab278@nyu.edu>
2017-11-16 18:08:40 -0500

    Get JRUBY OUTTAHERE

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ require 'formaggio/capistrano/server/puma'
 set :app_title, "eshelf"
 
 # Overriding defaults
-set :rvm_ruby_string, "jruby-9.1.7.0"
+set :rvm_ruby_string, "ruby-2.3.0"
 set :stages, ["staging", "qa", "production"]
 set :recipient, "lib-eshelf-admin@nyu.edu"
 set :puma_threads, '40:80'
