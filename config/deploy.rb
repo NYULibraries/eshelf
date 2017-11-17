10550ce6c26b5409a20d4cd51c08047f1bdffb0f
hab278 <hab278@nyu.edu>
2017-11-17 15:34:32 -0500

    Use passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,5 @@
 # Default require
 require 'formaggio/capistrano'
-# Specifying server to puma
-require 'formaggio/capistrano/server/puma'
 
 # Required variables
 set :app_title, "eshelf"
@@ -10,4 +8,3 @@ set :app_title, "eshelf"
 set :rvm_ruby_string, "ruby-2.3.3"
 set :stages, ["staging", "qa", "production"]
 set :recipient, "lib-eshelf-admin@nyu.edu"
-set :puma_threads, '40:80'
