62c5725f02148b8074362d2483863d79b3c88d81
hab278 <hab278@nyu.edu>
2017-11-17 15:30:36 -0500

    change ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ require 'formaggio/capistrano/server/puma'
 set :app_title, "eshelf"
 
 # Overriding defaults
-set :rvm_ruby_string, "ruby-2.3.0"
+set :rvm_ruby_string, "ruby-2.3.3"
 set :stages, ["staging", "qa", "production"]
 set :recipient, "lib-eshelf-admin@nyu.edu"
 set :puma_threads, '40:80'
