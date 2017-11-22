9d4b0e388d55db58e616a294638b1da242b79810
Barnaby Alter <barnaby.alter@nyu.edu>
2017-11-22 15:03:57 -0500

    Change views to match new nyulibraries_templates gem

---
+++ app/helpers/layouts_helper.rb
@@ -0,0 +1,37 @@
+module LayoutsHelper
+
+  def gauges_tracking_code
+    ENV['GAUGES_TOKEN']
+  end
+
+  # Add e-Shelf to the bread crumbs
+  def breadcrumbs
+    super << "e-Shelf"
+  end
+
+  # Back to icon and link
+  def back_to
+    (icon_tag(:back) + link_to_back) if link_to_back
+  end
+
+  # Back to link
+  def link_to_back
+    link_to wayfinder.text, wayfinder.url if wayfinder.match?
+  end
+
+  # Yep, we have a print stylesheet.
+  def print_stylesheet?
+    true
+  end
+
+  def print_title
+    t('application.print_title')
+  end
+
+  # Print stylesheet is assumed to be in assets/stylesheets/print.css.
+  # Media type is all to to render like it will print in most (all?) browsers.
+  def print_stylesheet
+    @_view.print_stylesheet :all
+  end
+
+end
