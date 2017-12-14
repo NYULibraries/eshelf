bea9256b5824cabf6d4e128f362c01c342a31f49
Barnaby Alter <barnaby.alter@nyu.edu>
2017-12-14 12:36:40 -0500

    Only precompile application_INST.css files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,13 +5,14 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w(
   print.js
   records.js
+  print.css
 )
 
-Rails.application.config.assets.precompile += [/.*\.js/,/.*\.(s?)css/]
+# Precompile all application_{inst}.css assets
+Rails.application.config.assets.precompile += [/application_(.+)\.css/]
 
 # All images
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
