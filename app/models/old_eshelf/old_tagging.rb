e58088354f87617dff2c1661a31119c6fb981171
BAlter <barnaby.alter@nyu.edu>
2018-08-14 14:20:58 -0400

    Removed old eshelf references since it is long since retired

--- app/models/old_eshelf/old_tagging.rb
+++
@@ -1,7 +0,0 @@
-module OldEshelf
-  class OldTagging < OldBase
-    belongs_to :old_record, foreign_key: 'taggable_id'
-    belongs_to :old_tag, foreign_key: 'tag_id'
-    self.table_name = "taggings"
-  end
-end
