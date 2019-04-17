746fa346cf472430a25d93d275a4e2cb90ec9d5f
BAlter <barnaby.alter@nyu.edu>
2019-04-17 11:49:15 -0400

    Lock rake version in

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,5 +7,4 @@ set :app_title, "eshelf"
 # Overriding defaults
 set :rvm_ruby_string, "ruby-2.5.5"
 set :stages, ["staging", "qa", "production"]
-set :recipient, "lib-eshelf-admin@nyu.edu"
 set :new_relic_environments, ["none"]
