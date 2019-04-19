3a816927999c8ad1a511654b8ab8d2318fecc9fb
BAlter <barnaby.alter@nyu.edu>
2019-04-19 15:57:03 -0400

    Ran rails app:update and remediated issues

--- config/application.rb
+++ config/application.rb
@@ -1,35 +1,26 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
-
-require 'figs'
-# Don't run this initializer on CIRCLECI.
-Figs.load(stage: Rails.env) unless ENV['CIRCLECI']
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require 'figs'
+# Don't run this initializer on CIRCLECI.
+Figs.load(stage: Rails.env) unless ENV['CIRCLECI']
+
 module Eshelf
   EXAMPLE_ORIGIN = 'example.com'
 
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
 
     # Cross Origin Request support
     # Set to a dummy value for tests
-    whitelisted_origins = Rails.env.test? ?
+    whitelisted_origins = Rails.env.test? ? 
       Eshelf::EXAMPLE_ORIGIN : (Figs.env['ESHELF_ORIGINS'] || [])
 
     config.middleware.insert_before 0, Rack::Cors do
@@ -42,6 +33,10 @@ module Eshelf
 
     # Default Mailer Host
     config.action_mailer.default_url_options = {host: 'https://eshelf.library.nyu.edu'}
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
 
