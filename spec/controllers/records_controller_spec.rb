745b2e6230a9f22b10fbeefc725ce72374c269b7
BAlter <barnaby.alter@nyu.edu>
2019-04-19 13:00:45 -0400

    Revert to forcing records and usrs to be prepopulated before runing this test

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -27,17 +27,17 @@ describe RecordsController do
   describe '#getit' do
     before(:each) { allow_any_instance_of(RecordsController).to receive(:current_user).and_return(user) }
     before(:each) { allow_any_instance_of(Eshelf::PnxJson).to receive(:openurl).and_return(record.url) }
-    let(:record)  { FactoryBot.create(:user_record, :primo, data: "data") }
+    let!(:record)  { FactoryBot.create(:user_record, :primo, data: "data") }
     subject { get :getit, params: { id: record.id } }
     context 'when the user is an NYU user' do
-      let(:user)    { FactoryBot.create(:user) }
+      let!(:user)    { FactoryBot.create(:user) }
       it 'should redirect to a getit', :vcr do
         expect(response).to redirect_to("https://dev.getit.library.nyu.edu/nyu/resolve?#{record.url}")
         expect(response.headers['X-CSRF-Token']).to be_nil
       end
     end
     context 'when the user is not an NYU user' do
-      let(:user)    { FactoryBot.create(:nysid_user) }
+      let!(:user)    { FactoryBot.create(:nysid_user) }
       it 'should redirect to a configuration defined persistent linker' do
         expect(response).to redirect_to("#{ENV['PERSISTENT_LINKER_URL']}#{record.external_id}")
         expect(response.headers['X-CSRF-Token']).to be_nil
