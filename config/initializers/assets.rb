e9888eeda95a068f36b204faaa4c9c50be8e31c0
BAlter <barnaby.alter@nyu.edu>
2019-04-22 13:23:35 -0400

    Add back asset customizations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,15 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w(
+  print.js
+  records.js
+  print.css
+)
+
+# Precompile all application_{inst}.css assets
+Rails.application.config.assets.precompile += [/application_(.+)\.css/]
+
+# All images
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
