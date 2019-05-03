2f01306a88804aeab27ce094956d3e858f95b42b
BAlter <barnaby.alter@nyu.edu>
2019-05-03 14:15:32 -0400

    Revert cookie_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_eshelf_session', domain: {
-    production: ENV['LOGIN_COOKIE_DOMAIN'],
-    staging: ENV['LOGIN_COOKIE_DOMAIN'],
-    qa: ENV['LOGIN_COOKIE_DOMAIN']
-  }.fetch(Rails.env.to_sym, :all)
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, key: '_eshelf_session', domain: ENV['LOGIN_COOKIE_DOMAIN']
