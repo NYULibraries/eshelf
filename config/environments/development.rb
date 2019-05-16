e1ed6f4a88eb51167d1f1b9d8b40909037527203
BAlter <barnaby.alter@nyu.edu>
2019-05-16 16:07:44 -0400

    Explicitly disable same origin policy per environment since the global config does not work; we handle a whitelist of origins

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_controller.forgery_protection_origin_check = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_controller.forgery_protection_origin_check = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
