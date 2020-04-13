9e02e4a7b9c7d3e142fd144ab557d2c25b9c4161
BAlter <barnaby.alter@nyu.edu>
2020-04-13 17:07:49 -0400

    Tests out using regexp for cors whitelist domain

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ Bundler.require(*Rails.groups)
 module Eshelf
   EXAMPLE_ORIGIN = 'example.com'
   # ESHELF_ORIGINS = [ENV['BOBCAT_ORIGIN_REGEX'], ENV['BOBCAT_ORIGIN_REGEX']].reject(&:nil?)
-  ESHELF_ORIGINS = ["bobcatdev.library.nyu.edu", "bobcat.library.nyu.edu"]
+  # ESHELF_ORIGINS = ["bobcatdev.library.nyu.edu", "bobcat.library.nyu.edu"]
+  ESHELF_ORIGINS = [/\A(http(s)?:\/\/)?bobcat(dev)?\.library\.nyu\.edu(:(\d){2,4})?\z/]
 
   class Application < Rails::Application
     config.eager_load_paths << Rails.root.join('lib')
