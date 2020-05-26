70e7949efdffd24b1cf9faf1e9b8a9f80beaf8ec
BAlter <barnaby.alter@nyu.edu>
2020-05-26 09:49:50 -0400

    Use CDN for asset host across environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,11 +32,6 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  if ENV['DOCKER'] && ENV['CDN_URL']
-    config.action_controller.asset_host = ENV['CDN_URL']
-  end
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
