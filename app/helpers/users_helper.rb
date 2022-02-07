4ef5d14e405dd8d945f583c1a0d103c293a57d0d
Eric Griffis <eric.griffis@nyu.edu>
2022-02-07 15:02:20 -0500

    Correct whitespace

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,13 @@
 module UsersHelper
   # Return a link to records filtered by the given tag
   def link_to_tagged_records(tag)
-  unless tag.name == t('record.tag_list.no_tags')
+    unless tag.name == t('record.tag_list.no_tags')
       link_to tag, records_url(tag: tag.name)
     else
       tag.name
     end
   end
+
   def aleph_account_url
     URI.escape("#{ENV['ALEPH_HTTPS_BASE_URL']}/F?func=bor-info")
   end
